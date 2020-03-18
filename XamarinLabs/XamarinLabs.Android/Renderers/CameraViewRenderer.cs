36e6ca3421e89cabc3d1021a7bbd4661c4313ef1
Rimaz <rimazmohommed523@gmail.com>
2020-03-18 13:53:46 +1300

    add android camera renderer

--- XamarinLabs/XamarinLabs.Android/Renderers/CameraViewRenderer.cs
+++ XamarinLabs/XamarinLabs.Android/Renderers/CameraViewRenderer.cs
@@ -1,17 +1,58 @@
-﻿using System.Runtime.Remoting.Contexts;
+﻿using System;
+using Android.Content;
+using Android.Hardware;
+using XamarinLabs.Droid;
+using XamarinLabs.Droid.Renderers;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 using XamarinLabs.Controls;
-using XamarinLabs.Droid.Renderers;
 
-[assembly: ExportRenderer(typeof(CameraView), typeof(CameraViewRenderer))]
+[assembly: ExportRenderer(typeof(CameraPreview), typeof(CameraViewRenderer))]
 namespace XamarinLabs.Droid.Renderers
 {
-    public class CameraViewRenderer : ViewRenderer
+    public class CameraViewRenderer : ViewRenderer<CameraView, CameraPreview>
     {
-        public CameraViewRenderer()
+        CameraPreview cameraPreview;
+        public CameraViewRenderer(Context context) : base(context)
         { 
         
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<CameraView> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+                cameraPreview = new CameraPreview(Context);
+                SetNativeControl(cameraPreview);
+            }
+            Control.Preview = Camera.Open((int)e.NewElement.Camera);
+            cameraPreview.Click += CameraPreview_Click;
+            if (e.OldElement != null)
+                cameraPreview.Click -= CameraPreview_Click;
+        }
+
+        private void CameraPreview_Click(object sender, EventArgs e)
+        {
+            if (cameraPreview.IsPreviewing)
+            {
+                cameraPreview.Preview.StopPreview();
+                cameraPreview.IsPreviewing = false;
+            }
+            else
+            {
+                cameraPreview.Preview.StartPreview();
+                cameraPreview.IsPreviewing = true;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (disposing)
+            {
+                Control.Preview.Release();
+            }
         }
     }
 }
